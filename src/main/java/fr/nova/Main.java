1c4845db234b9d8a30c323d6bb3af1417539e3b4
Sagby <sbouhdjeur.gagny@gmail.com>
2025-03-21 21:06:15 +0100

    test 2

--- src/main/java/fr/nova/Main.java
+++ src/main/java/fr/nova/Main.java
@@ -2,6 +2,6 @@ package fr.nova;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test submodule !");
+        System.out.println("Test submodule numero 2!");
     }
 }
